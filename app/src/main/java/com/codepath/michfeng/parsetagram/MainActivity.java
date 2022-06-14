4ef4c5e64df52ed01abce6498ff6f15933a321ad
Michelle Feng <michfeng@fb.com>
2022-06-14 13:36:34 -0700

    querying Parse database

--- app/src/main/java/com/codepath/michfeng/parsetagram/MainActivity.java
+++ app/src/main/java/com/codepath/michfeng/parsetagram/MainActivity.java
@@ -3,12 +3,59 @@ package com.codepath.michfeng.parsetagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogout;
+    private static final String TAG = "MainActivity";
+    private EditText etDescription;
+    private Button btnCaptureImage;
+    private ImageView ivPostImage;
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogout = findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOutInBackground();
+                ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
+            }
+        });
     }
+
+    private void queryPosts () {
+        // specify which class to query
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG,"Issue with getting posts",e);
+                    return;
+                }
+                for (Post post : posts) {
+                    Log.i(TAG,"Post: "+post.getDescription()+", username: " + post.getUser().getUsername());
+                }
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/codepath/michfeng/parsetagram/ParseApplication.java
+++ app/src/main/java/com/codepath/michfeng/parsetagram/ParseApplication.java
@@ -3,11 +3,15 @@ package com.codepath.michfeng.parsetagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register parse models
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("sRWg3HB1JvKhP0Y67fdOSQ7WhHUfdj0hWWDTW5R3")
